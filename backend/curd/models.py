3cc8f79fc14469f9d1ffd5e641ac9007e0f480cb
suhailvs <suhailvs@gmail.com>
2020-04-05 08:56:37 +0530

    added modified field in models

--- backend/curd/models.py
+++ backend/curd/models.py
@@ -5,9 +5,10 @@ class UserDetails(models.Model):
     firstName = models.CharField(max_length=255)
     email = models.CharField(max_length=255, blank=True)
     lastName = models.CharField(max_length=255)
+    modified = models.DateTimeField(auto_now=True)
 
 from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserDetails
-        fields = "__all__"
+        fields = ['id', 'firstName', 'email', 'lastName']
--- backend/curd/views.py
+++ backend/curd/views.py
@@ -1,27 +1,6 @@
-from django.shortcuts import render
-from rest_framework import generics,viewsets, mixins
+from rest_framework import viewsets
 from .models import UserSerializer, UserDetails
 
-#https://spapas.github.io/2018/05/08/django-reponse-add-delay/
-import time
-class TimeDelayMixin(object, ):
-    def dispatch(self, request, *args, **kwargs):
-        time.sleep(2)
-        return super().dispatch(request, *args, **kwargs)
-
-class CreateListRetrieveViewSet(TimeDelayMixin,mixins.CreateModelMixin,
-                                mixins.ListModelMixin,
-                                mixins.RetrieveModelMixin,
-                                mixins.UpdateModelMixin,
-                                mixins.DestroyModelMixin,
-                                viewsets.GenericViewSet):    
-    pass
-
-class UserListAPIView(CreateListRetrieveViewSet):
-    queryset = UserDetails.objects.all()
+class UserListAPIView(viewsets.ModelViewSet):
+    queryset = UserDetails.objects.order_by('-modified')
     serializer_class = UserSerializer
-
-    def create(self, request, *args, **kwargs):
-        print (request.data)
-        #return super().to_representation(instance)
-        return super(UserListAPIView, self).create(request, *args, **kwargs)
